f6f1bc2dd910027d359fe32b5c581e3ee721ae5a
Administrator <287368384@qq,com>
2018-08-30 10:46:38 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.gradle/4.1/fileHashes/fileHashes.bin
    #	.gradle/4.1/fileHashes/fileHashes.lock
    #	.gradle/4.1/fileHashes/resourceHashesCache.bin
    #	.gradle/4.1/javaCompile/classAnalysis.bin
    #	.gradle/4.1/javaCompile/jarAnalysis.bin
    #	.gradle/4.1/javaCompile/javaCompile.lock
    #	.gradle/4.1/javaCompile/taskHistory.bin
    #	.gradle/4.1/javaCompile/taskJars.bin
    #	.gradle/4.1/taskHistory/fileSnapshots.bin
    #	.gradle/4.1/taskHistory/taskHistory.bin
    #	.gradle/4.1/taskHistory/taskHistory.lock
    #	vo/AndroidManifest.xml
    #	vo/src/com/zl/vo_/main/activity/WelcomeActivity.java
    #	vo/vo.iml

---
+++ vo/src/com/zl/vo_/main/activity/TextActivity.java
@@ -0,0 +1,16 @@
+package com.zl.vo_.main.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+
+import com.zl.vo_.R;
+
+public class TextActivity extends Activity {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+      //  setContentView(R.layout.test);
+    }
+}
