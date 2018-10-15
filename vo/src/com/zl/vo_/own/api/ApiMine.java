58ae5341ccdc01c505fc0cf7425e106dfd662d58
Administrator <287368384@qq,com>
2018-10-15 10:00:15 +0800

    提交 -- 防漏提交

--- vo/src/com/zl/vo_/own/api/ApiMine.java
+++ vo/src/com/zl/vo_/own/api/ApiMine.java
@@ -1,5 +1,15 @@
 package com.zl.vo_.own.api;
 
+import android.app.Dialog;
+import android.content.Context;
+
+import com.zl.vo_.own.listener.OnRequestDataListener;
+import com.zl.vo_.own.util.WeiboDialogUtils;
+
+import java.util.Map;
+
+import static com.zl.vo_.own.api.ApiAccount.excutePost;
+
 /**
  * Created by Administrator on 2018/8/23.
  */
@@ -52,4 +62,57 @@ public class ApiMine {
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /**
+     * xzy:接口
+     */
+    //设置签名
+    public static void setSignature(final Context context, Map<String,String> params, final OnRequestDataListener listener) {
+        Dialog dialog = WeiboDialogUtils.createLoadingDialog(context, "加载中");
+        excutePost(ApiConstant.REGISTER, context, params,dialog, listener);
+    }
 }
